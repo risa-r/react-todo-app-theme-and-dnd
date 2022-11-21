b9a58caf31c7aff8f6dbccfc56db841d4851b525
risa-r <110155821+risa-r@users.noreply.github.com>
2022-11-21 19:10:10 +0900

    edit, delete, togglecomplete function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,53 @@ import { useState } from "react";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 import Footer from "./components/Footer";
+import { nanoid } from "nanoid";
 
 export default function App({ tasks }) {
   const [todos, setTodos] = useState(tasks);
 
   const taskList = todos.map((todo) => (
-    <Todo id={todo.id} name={todo.name} completed={todo.completed} />
+    <Todo
+      id={todo.id}
+      name={todo.name}
+      key={todo.id}
+      completed={todo.completed}
+      editTask={editTask}
+      deleteTask={deleteTask}
+      handleToggleCompleted={handleToggleCompleted}
+    />
   ));
 
   function addTask(name) {
-    const newTask = { id: `todo-${nextId++}`, name: name, completed: false };
+    const newTask = { id: `todo-${nanoid()}`, name: name, completed: false };
     setTodos([...todos, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editedTasks = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, name: newName };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(editedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = todos.filter((todo) => todo.id !== id);
+    setTodos(remainingTasks);
+  }
+
+  function handleToggleCompleted(id) {
+    const updatedTasks = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(updatedTasks);
   }
 
   return (
@@ -27,5 +63,3 @@ export default function App({ tasks }) {
     </>
   );
 }
-
-let nextId = 3;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,49 @@
-export default function Todo({ id, name, completed }) {
-  return (
-    <li key={id}>
-      <input type="checkbox" checked={completed} />
-      <label>{name}</label>
-      <button>Edit</button>
-      <button>Delete</button>
-    </li>
+import { useState } from "react";
+
+export default function Todo({
+  id,
+  name,
+  completed,
+  editTask,
+  deleteTask,
+  handleToggleCompleted,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [taskName, setTaskName] = useState(name);
+
+  function handleChange(e) {
+    setTaskName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    editTask(id, taskName);
+    setTaskName("");
+    setIsEditing(false);
+  }
+
+  const editTemplate = (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={taskName} onChange={handleChange}></input>
+      <label>
+        <span className="visually-hidden">edit the task name of '{name}'</span>
+      </label>
+      <button type="submit">Save</button>
+    </form>
   );
+
+  const viewTemplate = (
+    <>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => handleToggleCompleted(id)}
+      />
+      <label>{name}</label>
+      <button onClick={() => setIsEditing(true)}>Edit</button>
+      <button onClick={() => deleteTask(id)}>Delete</button>
+    </>
+  );
+
+  return <li key={id}>{isEditing ? editTemplate : viewTemplate}</li>;
 }
